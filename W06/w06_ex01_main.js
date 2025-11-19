42e1830a65dbf3eaaf3fff875d6f0a57d90d9def
Kaho Takenouchi <251x040x@stu.kobe-u.ac.jp>
2025-11-19 11:12:53 +0900

    Add W06

---
+++ W06/w06_ex01_main.js
@@ -0,0 +1,18 @@
+d3.csv("https://TakenouchiKaho.github.io/InfoVis2025/W04/data.csv")
+    .then( data => {
+        ShowScatterPlot(data);
+    })
+    .catch( error => {
+        console.log( error );
+    });
+
+function ShowScatterPlot(data) {
+    var svg = d3.select("body").append("svg");
+    svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+        .attr("r", d => d.r);
+};
